aec7776a9fab0c443b3a0e57cea667ebae47864c
xXH4CKST3RXx <52459831+xXH4CKST3RXx@users.noreply.github.com>
2019-09-29 19:57:53 -0400

    hi

--- js/stockPredictorTest.js
+++ js/stockPredictorTest.js
@@ -3,10 +3,10 @@ var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var myGlobalVars = {}
 let request = new XMLHttpRequest()
 let ticker = "AAPL"
-request.open('GET', `https://financialmodelingprep.com/api/v3/historical-price-full/${ticker}`,true)
+request.open('GET', `https://financialmodelingprep.com/api/v3/historical-price-full/AAPL`,true)
 request.onload = function() {
   // Begin accessing JSON data here
-  let data = JSON.parse(this.response)
+  let data = this.response
   let dailyStats = data.historical.reverse();
   console.log(dailyStats)
 
