ed89305e65b0111a830a6acfb46eb1103c57cc73
xXH4CKST3RXx <52459831+xXH4CKST3RXx@users.noreply.github.com>
2019-09-29 20:10:13 -0400

    h

--- js/stockPredictorTest.js
+++ js/stockPredictorTest.js
@@ -1,13 +1,24 @@
 const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-const xhr = new XMLHttpRequest();
-
-// completion callback
-xhr.addEventListener("load", () => {
-    console.log(xhr);
-    console.log(xhr.responseText);
-    const data = JSON.parse(xhr.responseText);
-    console.log(data.market_ask);
-});
-
-xhr.open("GET", "https://api.quoine.com/products/1");
-xhr.send();
+new Promise((resolve, reject) =>
+    {
+        const xhr = new XMLHttpRequest();
+        xhr.addEventListener("load", () => {
+            try {
+                if (xhr.status !== 200)
+                    throw new Error(`request failed with ${xhr.status}`);
+                const response = JSON.parse(xhr.responseText);
+                resolve(response);
+            } catch (e) {
+                reject(e);
+            }
+        });
+        xhr.addEventListener("error", reject);
+        xhr.open("GET", "https://api.quoine.com/products/1");
+        xhr.send();
+    })
+    .then(response => {
+        console.log("Market ask", response.market_ask);
+    })
+    .catch(error => {
+        console.log(error.message);
+    });
