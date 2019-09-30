a43049f60b2a7d3f9f62e28b681917ebae0e596f
xXH4CKST3RXx <52459831+xXH4CKST3RXx@users.noreply.github.com>
2019-09-29 21:48:57 -0400

    need to reinstall vscode after this

--- js/stockPredictorTest.js
+++ js/stockPredictorTest.js
@@ -1,7 +1,53 @@
 const axios = require('axios')
-
+var brain = require('brain')
 axios.get('https://financialmodelingprep.com/api/v3/historical-price-full/AAPL')
   .then((response) => {
     var a = response.data;
-    console.log(a.historical.reverse())
+    var dailyStats = a.historical.reverse();
+    // console.log(dailyStats)
+    function scaleDown(step) { // normalize
+        return {
+            open: step.open / 138,
+            high: step.high / 138,
+            low: step.low / 138,
+            close: step.close / 138
+        };
+      }
+    
+      function scaleUp(step) { // denormalize
+          return {
+            open: step.open * 138,
+            high: step.high * 138,
+            low: step.low * 138,
+            close: step.close * 138
+        };
+      }
+    
+      const scaledData = dailyStats.map(scaleDown);
+    
+      const trainingData = [
+        scaledData.slice(0, 5),
+        scaledData.slice(5, 10),
+        scaledData.slice(10, 15),
+        scaledData.slice(15, 20),
+      ];
+    
+      const net = new brain.recurrent.LSTMTimeStep({
+        inputSize: 4,
+        hiddenLayers: [8, 8],
+        outputSize: 4
+      });
+    
+      net.train(trainingData, {
+        learningRate: 0.005,
+        errorThresh: 0.02,
+        // log: (stats) => console.log(stats)
+      });
+    
+      // console.log(scaleUp(net.run(trainingData[0])));
+    
+      console.log(net.forecast([
+        trainingData[0][0],
+        trainingData[0][1],
+      ], 3).map(scaleUp));
   });
