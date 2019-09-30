d496b35028cd22b38e491c4b1e0d86194f11e91b
xXH4CKST3RXx <52459831+xXH4CKST3RXx@users.noreply.github.com>
2019-09-29 20:16:54 -0400

    h

--- js/stockPredictorTest.js
+++ js/stockPredictorTest.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch');
 
 
-fetch('https://api.github.com/users/github')
+fetch('https://financialmodelingprep.com/api/v3/historical-price-full/AAPL?serietype=line')
     .then(res => res.json())
     .then(json => console.log(json));
