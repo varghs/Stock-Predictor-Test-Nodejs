300b82c3adc29a0d34c3ea2be53a4b54d9992afd
xXH4CKST3RXx <52459831+xXH4CKST3RXx@users.noreply.github.com>
2019-09-29 20:18:48 -0400

    h

--- js/stockPredictorTest.js
+++ js/stockPredictorTest.js
@@ -1,6 +1,5 @@
-const fetch = require('node-fetch');
+let data = [];
 
-
-fetch('https://financialmodelingprep.com/api/v3/historical-price-full/AAPL?serietype=line')
-    .then(res => res.json())
-    .then(json => console.log(json));
+fetch("https://financialmodelingprep.com/api/v3/historical-price-full/AAPL?serietype=line")
+    .then(response => response.json())
+    .then(result => data.push(result));
