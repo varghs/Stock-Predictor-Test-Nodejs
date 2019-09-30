c81b0d5b814b071cba5a1509f6b8c715eab63841
xXH4CKST3RXx <52459831+xXH4CKST3RXx@users.noreply.github.com>
2019-09-29 20:20:01 -0400

    h

--- js/stockPredictorTest.js
+++ js/stockPredictorTest.js
@@ -4,3 +4,5 @@ let data = [];
 fetch("https://financialmodelingprep.com/api/v3/historical-price-full/AAPL?serietype=line")
     .then(response => response.json())
     .then(result => data.push(result));
+
+console.log(data)
